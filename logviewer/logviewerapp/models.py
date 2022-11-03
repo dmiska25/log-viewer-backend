e81ba5bf7c519e14aca8df046fe6297d3ac41d39
dmiska25 <73676469+dmiska25@users.noreply.github.com>
2022-11-02 22:50:08 -0500

    updated model date format and updated views

--- logviewer/logviewerapp/models.py
+++ logviewer/logviewerapp/models.py
@@ -24,7 +24,7 @@ class Log(models.Model):
 
     _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     service_name = models.CharField(max_length=100)
-    timestamp = models.TimeField()
+    timestamp = models.DateTimeField()
     severity = models.CharField(
         max_length=1,
         choices=Severity.choices,
--- logviewer/logviewerapp/views.py
+++ logviewer/logviewerapp/views.py
@@ -5,11 +5,24 @@ from .serializers import LogSerializer
 
 from .models import Log
 from . import urls
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, status, permissions
+from rest_framework.response import Response
+from django.shortcuts import render
 
 # Create your views here.
 class LogViewSet(viewsets.ModelViewSet):
     queryset = Log.objects.all()
     serializer_class = LogSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data,list))
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+def LogViewerHomePage(request):
+    return render(request, 'homepage.html')
+    
