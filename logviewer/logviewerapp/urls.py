066d275ee7ae0dca4a7666fd21f3aca83e75853b
dmiska25 <73676469+dmiska25@users.noreply.github.com>
2022-11-07 23:06:46 -0600

    refactors viewer to utilize htmx, adds dynamic pagination

--- logviewer/logviewerapp/urls.py
+++ logviewer/logviewerapp/urls.py
@@ -2,6 +2,8 @@ from tkinter.font import names
 from . import views
 from django.urls import include, path
 from rest_framework import routers
+from django.views.generic.base import RedirectView
+from datetime import datetime
 
 router = routers.DefaultRouter()
 router.register(r'logs', views.LogViewSet)
@@ -10,5 +12,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/auth', include('rest_framework.urls', namespace='rest_framework')),
 
-    path('log-viewer/', views.LogViewerHomePage)
+    path('log-viewer/', RedirectView.as_view(url='log-viewer/listing', permanent=True)),
+    path('log-viewer/listing/', views.listing)
 ]
--- logviewer/logviewerapp/views.py
+++ logviewer/logviewerapp/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 
@@ -9,6 +9,10 @@ from . import urls
 from rest_framework import viewsets, status, permissions
 from rest_framework.response import Response
 from django.shortcuts import render
+from datetime import datetime
+from django.core.paginator import Paginator
+
+PAGINATION_LIMIT = 30
 
 # Create your views here.
 class LogViewSet(viewsets.ModelViewSet):
@@ -23,11 +27,23 @@ class LogViewSet(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-
-def LogViewerHomePage(request):
-    logs = Log.objects.all().order_by('-timestamp')
+def listing(request):
+    time_before = datetime.fromisoformat(request.GET.get('time_before', datetime.now().isoformat()))
+    page = int(request.GET.get('page', 1))
+    logs = Log.objects.all().order_by('-timestamp').filter(timestamp__lt=time_before)
+    paginator = Paginator(logs, PAGINATION_LIMIT)
+    page_response = paginator.get_page(page)
+    
     args = {}
-    args['logs'] = logs
-    return TemplateResponse(request, "listing.html", args)
+    args['component'] = "listing"
+    args['logs'] = page_response
+    args['load_more_link'] = f"?time_before={time_before.isoformat()}&page={page+1}"
+    args['page'] = page
+    
+    if request.htmx:
+        if page > 1:
+            return TemplateResponse(request, "listing/listing_results.html", args)
+        return TemplateResponse(request, "listing/listing.html", args)
+    return TemplateResponse(request, "homepage.html", args)
 
-def LogVewerRequest(request):
+
